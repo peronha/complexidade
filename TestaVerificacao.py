d5f11504ce64aa038866c530d0d2070e3b5e597c
Pedro <pedro.ceriotti@hotmail.com>
2018-05-27 22:00:28 -0300

    Teste unitario para a verificacao.

--- TestaVerificacao.py
+++ TestaVerificacao.py
@@ -1,41 +1,41 @@
 from verificacao import verificaSubSetSum
+import unittest
 
-def main():
-    S = [3,1,1,2,2,1]
-    S1 = [1,1,1,2]
-    S2 =  [2,3]
+class TestSubsetSum(unittest.TestCase):
 
-    print(verificaSubSetSum(S,S1,S2))
+    def test_all(self):
+        S = [3, 1, 1, 2, 2, 1]
+        S1 = [1, 1, 1, 2]
+        S2 = [2, 3]
 
-    S1 = [3, 1, 1]
-    S2 = [2, 2, 1]
+        self.assertEqual(verificaSubSetSum(S, S1, S2), True)
 
-    print(verificaSubSetSum(S,S1,S2))
+        S1 = [3, 1, 1]
+        S2 = [2, 2, 1]
 
-    S1 = [5, 1, 1]
-    S2 = [2, 2, 1]
+        self.assertEqual(verificaSubSetSum(S, S1, S2), True)
 
-    print(verificaSubSetSum(S,S1,S2))
+        S1 = [5, 1, 1]
+        S2 = [2, 2, 1]
 
+        self.assertEqual(verificaSubSetSum(S, S1, S2), False)
 
-    S1 = [1, 1, 1]
-    S2 = [1, 1, 1]
+        S1 = [1, 1, 1]
+        S2 = [1, 1, 1]
 
-    print(verificaSubSetSum(S,S1,S2))
+        self.assertEqual(verificaSubSetSum(S, S1, S2), False)
 
-    S1 = [2, 1, 1]
-    S2 = [3, 2, 1]
+        S1 = [2, 1, 1]
+        S2 = [3, 2, 1]
 
-    print(verificaSubSetSum(S,S1,S2))
+        self.assertEqual(verificaSubSetSum(S, S1, S2), False)
 
-    S1 = [2, 1, 1]
-    S2 = [3, 1]
+        S1 = [2, 1, 1]
+        S2 = [3, 1]
 
-    print(verificaSubSetSum(S,S1,S2))
-
-
-    return
+        self.assertEqual(verificaSubSetSum(S, S1, S2), False)
 
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == '__main__':
+    unittest.main()
